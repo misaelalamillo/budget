b76624d13fd135e743fb1f2ded66de19e772d779
misaelalamillo <misaelalamillo@gmail.com>
2020-03-27 16:50:41 -0500

    checkdatabase

--- public/db.js
+++ public/db.js
@@ -29,3 +29,36 @@ request.onsuccess = function(event) {
 
   store.add(record)
 }
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  const store = transaction.objectStore(["pending"], "readwrite");
+
+    const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+      
+          const transaction = db.transaction(["pending"], "readwrite");
+
+          const store = transaction.objectStore(["pending"], "readwrite");
+ 
+          store.clear();
+      });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase);
+
