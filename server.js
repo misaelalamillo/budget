7f3167817ea004057125cb1de62d6c4668231410
misaelalamillo <misaelalamillo@gmail.com>
2020-03-28 00:37:20 -0500

    changed const for process.env

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+process.env.MONGODB_URI
 
 app.use(logger("dev"));
 
